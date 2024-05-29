18a7921517fac477cd40b93ba92097350739957d
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2024-05-28 23:08:38 -0700

    chore: Update navbar styling and structure

--- react-app/src/components/Nav.tsx
+++ react-app/src/components/Nav.tsx
@@ -1,20 +1,22 @@
-
-
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-dark  w-100 p-2 sticky-top">
+    <nav
+      className="navbar navbar-expand-md bg-dark w-100 px-5 sticky-top"
+      data-bs-theme="dark"
+    >
       <a className="navbar-brand" href="#">
-        <span className="text-white display-4">TC</span>
+        <span className=" display-5">TC</span>
       </a>
+
       <div className="collapse navbar-collapse" id="navCollapse">
-        <div className="navbar-nav">
-          <a className="nav-link text-white" href="#projects">
+        <div className="navbar-nav me-auto">
+          <a className="nav-link" href="#projects">
             Projects
           </a>
-          <a className="nav-link text-white" href="#experience">
+          <a className="nav-link" href="#experience">
             Experience
           </a>
-          <a className="nav-link text-white" href="#education">
+          <a className="nav-link" href="#education">
             Education
           </a>
         </div>
