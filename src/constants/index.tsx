1dc56682aeb91fe8c965961da7dca66ec8c9c9eb
0Thomas1 <0Thomas1@cs.sfu.ca>
2025-09-03 14:22:26 -0700

    added icon for KanBan MCP project

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,6 +2,35 @@ import { CardProps } from "../components/Card";
 import { InfoProps } from "../components/Experience";
 export const projects: CardProps[] = [
   {
+    img: "assets/Mcp_Icon.png",
+    title: "Kanban MCP",
+    description:
+      "Kanban-style task manager with Node.js, TypeScript, and MongoDB. Features MCP server for agent automation, robust APIs, Zod validation, JSON caching, and real-time updates.",
+    gitLink: "",
+    link: "https://vancouver-trails-api-5apnr.ondigitalocean.app/",
+  },
+  {
+    img: "https://placehold.co/600x400",
+    title: "Vancouver Trails API",
+    description:
+      "A simple API for Vancouver trails, providing information about various hiking trails in the Vancouver area.",
+    gitLink: "",
+    link: "https://vancouver-trails-api-5apnr.ondigitalocean.app/",
+  },{
+    img: "assets/kanban.jpg",
+    title: "Kanban Board",
+    description:
+      "A simple Kanban board website for tracking tasks. Features include account systems using MongoDB.",
+    gitLink: "https://github.com/0Thomas1/kanban",
+  },
+  {
+    img: "https://placehold.co/600x400",
+    title: "Pixoo kanban",
+    description:
+      "A simple kanban board display on a Pixoo 64 Led display. Using DB from the previous project.",
+    gitLink: "https://github.com/0Thomas1/Pixoo-Kanban",
+  },
+  {
     img: "assets/exit-logo.jpg",
     title: " Exit Cafe & Lounge webpage design",
     description: "Responsive design for a cafe & lounge using Bootstrap and js",
@@ -29,35 +58,7 @@ export const projects: CardProps[] = [
       "A simple to-do list website for tracking desired weapons in Monster Hunter World. Features include account systems using MongoDB, full access to MHW weapons via API, and total material tracking for your weapons.",
     gitLink: "https://github.com/Scansy/mhw-checklist",
   },
-  {
-    img: "assets/kanban.jpg",
-    title: "Kanban Board",
-    description:
-      "A simple Kanban board website for tracking tasks. Features include account systems using MongoDB.",
-    gitLink: "https://github.com/0Thomas1/kanban",
-  },
-  {
-    img: "https://placehold.co/600x400",
-    title: "Pixoo kanban",
-    description:
-      "A simple kanban board display on a Pixoo 64 Led display. Using DB from the previous project.",
-    gitLink: "https://github.com/0Thomas1/Pixoo-Kanban",
-  },
-  {
-    img: "https://placehold.co/600x400",
-    title: "Kanban MCP",
-    description:
-      "Kanban-style task manager with Node.js, TypeScript, and MongoDB. Features MCP server for agent automation, robust APIs, Zod validation, JSON caching, and real-time updates.",
-    gitLink: "",
-    link: "https://vancouver-trails-api-5apnr.ondigitalocean.app/",
-  },{
-    img: "https://placehold.co/600x400",
-    title: "Vancouver Trails API",
-    description:
-      "A simple API for Vancouver trails, providing information about various hiking trails in the Vancouver area.",
-    gitLink: "",
-    link: "https://vancouver-trails-api-5apnr.ondigitalocean.app/",
-  }
+  
 ];
 
 export const experience: InfoProps[] = [
