252f8c4e72d63f7ab54b1c2d83ea892f276fa104
0Thomas1 <0Thomas1@cs.sfu.ca>
2026-04-01 12:06:04 -0700

    feat: add filter state to Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import { projects } from "../constants";
 import Card from "./Card";
 const Projects = () => {
-return (
-    <div
-        className="col-10 container-fluid bg-gray text-center py-3"
-        id="projects"
-    >
-        <div className="row row-cols-md-3 row-cols-1 g-4 justify-content-center">
-            {projects.map((project) => (
-                <div key={project.title} className="d-flex justify-content-center">
-                    <Card {...project} />
-                </div>
-            ))}
+    const[filter, setFilter] = useState("All")
+    return (
+        <div
+            className="col-10 container-fluid bg-gray text-center py-3"
+            id="projects"
+        >
+            <div className="row row-cols-md-3 row-cols-1 g-4 justify-content-center">
+                {projects.map((project) => (
+                    <div key={project.title} className="d-flex justify-content-center">
+                        <Card {...project} />
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Projects;
